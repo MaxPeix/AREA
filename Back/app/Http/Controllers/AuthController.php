2fd45289c9af393fcc7638c7fa40786c5038d1e0
Gautier <bonhur.gautier@gmail.com>
2023-09-25 16:27:27 +0200

    [DEL] delete route logout because it's useless in the back

--- Back/app/Http/Controllers/AuthController.php
+++ Back/app/Http/Controllers/AuthController.php
@@ -100,15 +100,6 @@ class AuthController extends Controller
         ]);
     }
 
-    public function logout()
-    {
-        Auth::logout();
-        return response()->json([
-            'status' => 'success',
-            'message' => 'Successfully logged out',
-        ]);
-    }
-
     public function refresh()
     {
         return response()->json([
--- Back/routes/api.php
+++ Back/routes/api.php
@@ -26,5 +26,4 @@ Route::middleware('custom.auth')->group(function () {
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
-Route::post('/logout', [AuthController::class, 'logout']);
 Route::post('/refresh', [AuthController::class, 'refresh']);
