c4d8808e5ba66492889c3d6b6f5439328b9f118e
Gautier <bonhur.gautier@gmail.com>
2023-10-04 15:29:32 +0200

    [ADD] check of spotify and google token validity

---
+++ Back/app/Http/Controllers/CheckTokenService.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+
+class CheckTokenService extends Controller
+{
+    public function checkGoogleToken(User $user)
+    {
+        $user = Auth::user();
+        $googleToken = $user->google_token;
+
+        if (!$googleToken) {
+            return false;
+        }
+
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, "https://www.googleapis.com/oauth2/v3/tokeninfo?access_token={$googleToken}");
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, CURLOPT_HTTPGET, 1);
+
+        $response = curl_exec($ch);
+
+        if (curl_errno($ch)) {
+            curl_close($ch);
+            return response()->json(['message' => 'An error occurred while checking the Google token'], 500);
+        }
+        curl_close($ch);
+
+        $decodedResponse = json_decode($response, true);
+
+        if (isset($decodedResponse['error_description'])) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    public function checkSpotifyToken(User $user)
+    {
+        $user = Auth::user();
+        $spotifyToken = $user->spotify_token;
+
+        if (!$spotifyToken) {
+            return false;
+        }
+
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, "https://api.spotify.com/v1/me");
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt(
+            $ch,
+            CURLOPT_HTTPHEADER,
+            array(
+                'Authorization: Bearer ' . $spotifyToken
+            )
+        );
+        curl_setopt($ch, CURLOPT_HTTPGET, 1);
+
+        $response = curl_exec($ch);
+
+        if (curl_errno($ch)) {
+            curl_close($ch);
+            return response()->json(['message' => 'An error occurred while checking the Spotify token'], 500);
+        }
+        curl_close($ch);
+
+        $decodedResponse = json_decode($response, true);
+
+        if (isset($decodedResponse['error_description'])) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    public function checkTokensValidity () {
+        $user = Auth::user();
+        $google = $this->checkGoogleToken($user);
+        $spotify = $this->checkSpotifyToken($user);
+        return [
+            'google' => $google,
+            'spotify' => $spotify
+        ];
+    }
+}
