d881162319f987860ba437b82161c914d9230c76
Gautier <bonhur.gautier@gmail.com>
2023-10-04 10:28:31 +0200

    [FIX] change request to curl

--- Back/app/Http/Controllers/GoogleOAuthController.php
+++ Back/app/Http/Controllers/GoogleOAuthController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Http;
 use Firebase\JWT\JWT;
 use Carbon\Carbon;
 
@@ -14,42 +13,42 @@ class GoogleOAuthController extends Controller
         $clientId = "1025827948903-tlsoh7n6clf1hgggh752na4gpn0h0n4l.apps.googleusercontent.com";
         $clientSecret = "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";
         $scope = 'email profile';
-        $redirectUri = 'http://127.0.0.1:80/api';
+        $redirectUri = 'http://127.0.0.1:8000/api/oauth2callback';
 
         $code = $request->input('code');
 
         if (!$code) {
             $authUri = "https://accounts.google.com/o/oauth2/v2/auth?response_type=code&client_id={$clientId}&redirect_uri={$redirectUri}&scope={$scope}";
-            return redirect($authUri);
+            return json_encode([
+                'authUri' => $authUri,
+            ]);
         } else {
-            $response = Http::asForm()->post('https://oauth2.googleapis.com/token', [
+            $ch = curl_init("https://oauth2.googleapis.com/token");
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+            curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query([
                 'code' => $code,
                 'client_id' => $clientId,
                 'client_secret' => $clientSecret,
                 'redirect_uri' => $redirectUri,
                 'grant_type' => 'authorization_code'
+            ]));
+            curl_setopt($ch, CURLOPT_POST, 1);
+            $output = curl_exec($ch);
+            curl_close($ch);
+            
+            $credentials = json_decode($output, true);
+
+            $ch = curl_init("https://openidconnect.googleapis.com/v1/userinfo");
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+            curl_setopt($ch, CURLOPT_HTTPHEADER, [
+                'Authorization: Bearer ' . $credentials['access_token']
             ]);
-
-            $credentials = json_decode($response->body(), true);
-
-            $response = Http::withHeaders([
-                'Authorization' => 'Bearer ' . $credentials['access_token'],
-            ])->get('https://openidconnect.googleapis.com/v1/userinfo');
-
-            $userInfo = json_decode($response->body(), true);
-
-            $jwtPayload = [
-                'exp' => Carbon::now()->addHours(1)->timestamp,
-                'iat' => Carbon::now()->timestamp,
-                'sub' => $credentials['access_token'],
-                'data' => $userInfo,
-            ];
-
-            $jwtSecret = env('JWT_SECRET');
-            $jwtToken = JWT::encode($jwtPayload, $jwtSecret, 'HS256');
+            $output = curl_exec($ch);
+            curl_close($ch);
+            
+            $userInfo = json_decode($output, true);
 
             return json_encode([
-                'token' => $jwtToken,
                 'user' => $userInfo,
             ]);
         }
