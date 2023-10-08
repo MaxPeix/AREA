2694865fe76cdd6d9fa6511edb919c9fcabf44fb
Gautier <bonhur.gautier@gmail.com>
2023-10-08 17:08:48 +0200

    [DEL] area creator

--- Front/WEB/src/main.js
+++ Front/WEB/src/main.js
@@ -12,7 +12,6 @@ import Home from './components/Home.vue'
 import Account from './components/Account.vue'
 import Areas from './components/Areas.vue'
 import AreaEditor from './components/AreaEditor.vue';
-import AreaCreator from './components/AreaCreator.vue';
 
 Vue.config.productionTip = false
 Vue.use(Buefy);
@@ -25,7 +24,6 @@ const routes = [
   { path: '/home', component: Home, name: 'home', props: true},
   { path: '/account', component: Account, name: 'account' },
   { path: '/areaeditor', component: AreaEditor, name: 'areaeditor', props: true},
-  { path: '/areacreator', component: AreaCreator, name: 'areacreator'},
   { path: '/areas', component: Areas, name: 'areas', props: true},
   { path: '/:pathMatch(.*)*', redirect: '/' },
 ];
