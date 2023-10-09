65f72d697ccc1ec049f89965f1761d7f4dd0164f
Gautier <bonhur.gautier@gmail.com>
2023-10-09 15:40:28 +0200

    [FIX] message return error

--- Back/app/Console/Commands/mail_received_checks.php
+++ Back/app/Console/Commands/mail_received_checks.php
@@ -67,7 +67,7 @@ class mail_received_checks extends Command
         }
         $validity = $this->checkGoogleToken($user);
         if (!$validity) {
-            Log::info("google token not is valid for user: " . $user->id . " - " . $user);
+            Log::info("google token is not valid for user: " . $user->id . " - " . $user);
             return 1;
         }
         $googleToken = $user->google_token;
