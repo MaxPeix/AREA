916ced1a0c5f8b4fc63bb40b2471308f257edf22
Hugo Dubois <duboish00@gmail.com>
2023-10-11 14:59:15 +0200

    AREA-52 [FRONT] [ADD] connexion

---
+++ Front/MOBILE/MOBILE/Components/GoogleView.swift
@@ -0,0 +1,18 @@
+//
+//  GoogleView.swift
+//  MOBILE
+//
+//  Created by Hugo Dubois on 09/10/2023.
+//
+
+import SwiftUI
+
+struct GoogleView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GoogleView()
+}
