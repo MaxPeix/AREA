398a050639081880407446c41848c5b49531d023
Gautier <bonhur.gautier@gmail.com>
2023-10-31 12:20:02 +0100

    [ADD] about.json

--- Back/routes/web.php
+++ Back/routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Models\Service;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +17,58 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/about.json', function (Request $request) {
+    $clientIp = $request->ip();
+    $currentTime = time();
+
+    $services = Service::all()->toArray();
+
+    $serviceApplications = [];
+
+    foreach ($services as $service) {
+        $allowedApps = ['spotify', 'google', 'github', 'discord', 'reached'];
+        $appName = '';
+
+        foreach ($allowedApps as $allowedApp) {
+            if (stripos($service['service_name'], $allowedApp) !== false) {
+                $appName = $allowedApp;
+                break;
+            }
+        }
+
+        if (empty($appName)) {
+            continue;
+        }
+
+        $serviceType = (strpos($service['service_name'], '[ACTION]') !== false) ? 'actions' : 'reactions';
+
+        if (!isset($serviceApplications[$appName])) {
+            $serviceApplications[$appName] = [
+                'name' => ucfirst($appName),
+                'actions' => [],
+                'reactions' => []
+            ];
+        }
+
+        $serviceApplications[$appName][$serviceType][] = [
+            'name' => $service['service_name'],
+            'description' => $service['service_description']
+        ];
+    }
+
+    $serviceApplications = array_values($serviceApplications);
+
+    $aboutJson = [
+        'client' => [
+            'host' => $clientIp
+        ],
+        'server' => [
+            'current_time' => $currentTime,
+            'services' => $serviceApplications
+        ]
+    ];
+
+    return response()->json($aboutJson);
 });
